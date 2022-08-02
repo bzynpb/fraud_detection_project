e98089c0b9441bf98951330de298800798cd0115
bzynpb <90254686+bzynpb@users.noreply.github.com>
2022-08-02 17:36:11 +0100

    Update Fraud_App.py

--- Fraud_App.py
+++ Fraud_App.py
@@ -1,4 +1,5 @@
 import pickle
+import joblib
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -70,60 +71,74 @@ def set_background(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
 set_background('b1.jpeg')
 
-
+['V3', 'V4', 'V7',
+           
+ 'V10',
+	   'V11',
+           'V12',
+ 
+           'V14',
+	   'V16',
+           'V17',
+ 
 c1, c2, c3  = st.columns((3)) # ekrani 2 kolona ayirdik
 with c1:
-    V4=st.slider("Employee satisfaction point", 0.01, 1.00, 0.50, step=0.01)
-    V10=st.slider("Evaluated performance by the employer", 0.01, 1.00, 0.50, step=0.01)
-    
+	    V3=st.slider("V3", -48.32, 9.380, 0, step=0.05)
+	    V4=st.slider("V4", -5.68, 16.875, 0, step=0.05)
+	    V7=st.slider("V7", -43.557, 16.875, 50, step=1)
+
 with c2:
-    V12=st.slider("How many of projects assigned to an employee?", 2 ,7, 5, step=1)
-    V17=st.slider("How many hours in averega an employee worked in a month?", 96, 310, 200, step=1)
+	 V10=st.slider("V10", -24.588, 23.745, 0, step=0.05)
+	 V11=st.slider("V11", -4.797, 12.019, 0, step=0.05)
+	 V12=st.slider("V12", -18.684, 7.848, 0, step=0.05)
     
 with c3:
-    Amount=st.slider("How many hours in averega an employee worked in a month?", 96, 310, 200, step=1)
-
+ 	V14=st.slider("V14", -19.214, 10.527, 0, step=0.05)
+	V16=st.slider("V16", -14.130, 17.315, 0, step=0.05)
+	V17=st.slider("V17", -25.163, 9.254, 0, step=0.05)
+ 
 ## Slider color	
 ColorMinMax = st.markdown(''' <style> div.stSlider > div[data-baseweb = "slider"] > div[data-testid="stTickBar"] > div { background: rgb(1 1 1 / 0%); } </style>''', unsafe_allow_html = True)
 Slider_Cursor = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div[role="slider"]{ background-color: rgb(0, 51, 102); box-shadow: rgb(14 38 74 / 20%) 0px 0px 0px 0.3rem;} </style>''', unsafe_allow_html = True)
 Slider_Number = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div > div { color: rgb(14, 38, 74); } </style>''', unsafe_allow_html = True)
 
 col = f''' <style> div.stSlider > div[data-baseweb = "slider"] > div > div {{ background: linear-gradient(to right, rgb(1, 183, 158) 0%, 
-					rgb(14, 38, 74) {Amount}%, 
-					rgba(14, 38, 74, 0.25) {Amount}%, 
+					rgb(14, 38, 74) {V3}%, 
+					rgba(14, 38, 74, 0.25) {V3}%, 
 					rgba(14, 38, 74, 0.25) 100%); }} </style>'''
 
 ColorSlider = st.markdown(col, unsafe_allow_html = True)   
 
 
 
+model = joblib.load(open("rf_model.pkl", "rb"))
 
 
-
-
-model = pickle.load(open("model.pkl", "rb"))
-scaler = pickle.load(open("scaler.pkl", "rb"))
-
-
-
-coll_dict = {'V4':V4,
+coll_dict = {'V3':V3,
+	     'V4':V4,
+	     'V7':V7,
              'V10':V10,
+	     'V11':V11,
              'V12':V12,
-             'V14': V14,
+             'V14':V14,
+	     'V16':V16,
              'V17': V17,
-    	     'Amount':Amount
             }
 
-columns = ['V4',
-           'V10', 
+columns = ['V3',
+	   'V4',
+	   'V7',
+           'V10',
+	   'V11',
            'V12',
-           'V14', 
+           'V14',
+	   'V16',
            'V17',
-           'Amount'
           ]
 
 df_coll = pd.DataFrame.from_dict([coll_dict])
-prediction = model.predict(user_inputs)
+
+prediction = model.predict(df_coll)
 
 
 # def predict():
